fb7f8f456f8fc5fa0a8d46acd6c13e6a411a7172
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-02-27 18:50:01 +0800

    First commit

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,14 @@
+﻿using Firebase;
+using Firebase.Database;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{    
+    private void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            
+        });
+    }
+}
